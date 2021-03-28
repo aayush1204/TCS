71caecb2e180c5c335c97ab1d6179a5c3bde6834
aayush1204 <aayush.halgekar@gmail.com>
2021-03-28 16:52:34 +0530

    asas

--- lighthouse/views.py
+++ lighthouse/views.py
@@ -16,53 +16,6 @@ from .models import Laws, Score
 
 import requests
 
-q1 = 1
-q2 = 0
-q3 = 0
-q4 = 0
-q5 = 0
-
-overall_score = 0
-read_score = 0
-#utility function to read files (ToS) to an list of paragraphs.
-def read_file_to_paragraphs(file_path):
-    file = open(file_path, 'r', encoding="utf8")
-    doc = file.read()
-    file.close()
-    pars = re.split('\n\n+', doc) #some documents have a end of line during what could be considered the same 
-                                    #paragraph, I believe 2 or more \n's if a better slip for this.
-    print('reading %s wich have %d paragraphs' % (file_path, len(pars)))
-    return(pars)
-
-#utility function that will tokenize, remove stop-words and stem the paragraphs.
-def tokenize_and_stem(text, tokenizer, stemmer, stop_words):
-        return([stemmer.stem(word) for word in tokenizer.tokenize(text.lower()) if word not in stop_words])
-
-def create_topic_pars(pars, tokenizer, stemmer, stop_words, ldamodel, word_dictionary, topic_dictionary):
-    norm_pars = [tokenize_and_stem(par, tokenizer, stemmer, stop_words) for par in pars]
-    print('created normalized paragraphs object of length %d' % len(norm_pars))
-    bows = [word_dictionary.doc2bow(text) for text in norm_pars]
-    print('created bag-of-words object of length %d' % len(bows))
-    topic_pars = []
-    for idx, val in enumerate(bows):
-        lda_vector = ldamodel[val]
-        #original LDA model topic (most relevant) and paragraph:
-        topic_pars.append([ldamodel.print_topic(max(lda_vector, key=lambda item: item[1])[0]), pars[idx]]) #we attach the original paragraph here, not the cleaned version that we used for LDA.
-    
-    #now we'll create a nicely labeled structure.
-    tagged_pars = []
-    for topic_name in topic_dictionary:
-        topic_words = topic_dictionary[topic_name]
-        for pars in topic_pars:
-            topic = pars[0]
-            par = pars[1]
-            if(len(par) > 50):
-                for word in topic_words:
-                    if stemmer.stem(word) in topic:
-                        tagged_pars.append([par, topic_name])
-                        break
-    return(tagged_pars)
-
 # Importing all the necessary libraries
 import nltk, re, numpy as np
 import urllib
